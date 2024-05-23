790fa5e28d9509f8f3d01eaebf523234bd9a4f6a
d1204429 <d1204429@o365.fcu.edu.tw>
2024-05-23 13:21:35 +0800

    ver1.0.2

--- src/Main.java
+++ src/Main.java
@@ -12,40 +12,40 @@ public class Main {
     Car carGtr = newProduceCar.produceCar();
 
     System.out.println(
-        "車牌號碼" + carAlpha.getCarId() + "\n"
-            + "引擎號碼" + carAlpha.getEngineId() + "\n"
-            + "車身顏色" + carAlpha.getColor() + "\n");
+        "車牌號碼" + "\t" + carAlpha.getCarId() + "\n"
+            + "引擎號碼" + "\t" + carAlpha.getEngineId() + "\n"
+            + "車身顏色" + "\t" + carAlpha.getColor() + "\n");
     System.out.println(
-        "車牌號碼" + carNinja.getCarId() + "\n"
-            + "引擎號碼" + carNinja.getEngineId() + "\n"
-            + "車身顏色" + carNinja.getColor() + "\n");
+        "車牌號碼" + "\t" + carNinja.getCarId() + "\n"
+            + "引擎號碼" + "\t" + carNinja.getEngineId() + "\n"
+            + "車身顏色" + "\t" + carNinja.getColor() + "\n");
     System.out.println(
-        "車牌號碼" + carCybertruck.getCarId() + "\n"
-            + "引擎號碼" + carCybertruck.getEngineId() + "\n"
-            + "車身顏色" + carCybertruck.getColor() + "\n");
+        "車牌號碼" + "\t" + carCybertruck.getCarId() + "\n"
+            + "引擎號碼" + "\t" + carCybertruck.getEngineId() + "\n"
+            + "車身顏色" + "\t" + carCybertruck.getColor() + "\n");
     System.out.println(
-        "車牌號碼" + carGtr.getCarId() + "\n"
-            + "引擎號碼" + carGtr.getEngineId() + "\n"
-            + "車身顏色" + carGtr.getColor() + "\n");
+        "車牌號碼" + "\t" + carGtr.getCarId() + "\n"
+            + "引擎號碼" + "\t" + carGtr.getEngineId() + "\n"
+            + "車身顏色" + "\t" + carGtr.getColor() + "\n");
 
-    System.out.println("Car生產數量" + newProduceCar.getProducedCars());
-    System.out.println("Taxi生產數量" + newProduceTaxi.getProducedCars());
-    System.out.println("ElectricCar生產數量" + newProduceElectricCar.getProducedCars() + "\n");
+    System.out.println("Car生產數量" + "\t" + newProduceCar.getProducedCars());
+    System.out.println("Taxi生產數量" + "\t" + newProduceTaxi.getProducedCars());
+    System.out.println("ElectricCar生產數量" + "\t" + newProduceElectricCar.getProducedCars() + "\n");
 
-    System.out.println("更新車牌" + newProduceCar.updateCarId(carAlpha, "CCC-9966"));
-    System.out.println("更新車牌" + newProduceTaxi.updateCarId(carNinja, "TAB-7788"));
+    System.out.println("更新車牌" + "\t" + newProduceCar.updateCarId(carAlpha, "CCC-9966"));
+    System.out.println("更新車牌" + "\t" + newProduceTaxi.updateCarId(carNinja, "TAB-7788"));
     System.out.println(
-        "更新車牌" + newProduceElectricCar.updateCarId(carCybertruck, "ESC-0101") + "\n");
+        "更新車牌" + "\t" + newProduceElectricCar.updateCarId(carCybertruck, "ESC-0101") + "\n");
 
-    System.out.println("更新車牌" + newProduceCar.updateCarId(carAlpha, "AAA-1122"));
-    System.out.println("更新車牌" + newProduceTaxi.updateCarId(carNinja, "YAA-5656"));
+    System.out.println("更新車牌" + "\t" + newProduceCar.updateCarId(carAlpha, "AAA-1122"));
+    System.out.println("更新車牌" + "\t" + newProduceTaxi.updateCarId(carNinja, "YAA-5656"));
     System.out.println(
-        "更新車牌" + newProduceElectricCar.updateCarId(carCybertruck, "PIP-9527") + "\n");
+        "更新車牌" + "\t" + newProduceElectricCar.updateCarId(carCybertruck, "PIP-9527") + "\n");
 
-    System.out.println("更新顏色" + newProduceCar.updateCarColor(carAlpha, "White"));
-    System.out.println("更新顏色" + newProduceElectricCar.updateCarColor(carCybertruck, "Black"));
-    System.out.println("更新顏色" + newProduceCar.updateCarColor(carGtr, "Yellow"));
-    System.out.println("更新顏色" + newProduceElectricCar.updateCarColor(carCybertruck, "Red"));
+    System.out.println("更新顏色" + "\t" + newProduceCar.updateCarColor(carAlpha, "White"));
+    System.out.println("更新顏色" + "\t" + newProduceElectricCar.updateCarColor(carCybertruck, "Black"));
+    System.out.println("更新顏色" + "\t" + newProduceCar.updateCarColor(carGtr, "Yellow"));
+    System.out.println("更新顏色" + "\t" + newProduceElectricCar.updateCarColor(carCybertruck, "Red"));
 
   }
 }
